438daa0c0067743164b315065f6167b781ca0fca
Santiago Martinez <smartinez9626@gmail.com>
2025-09-10 22:19:57 -0500

    feat: añadir función seedCustomers para inicializar datos de clientes
    - Se ha añadido la función seedCustomers en el archivo seed.ts para incluir la inicialización de datos de clientes en la base de datos.
    - Se ha actualizado la función main para llamar a seedCustomers, mejorando la configuración inicial de datos.

--- prisma-tenant-base/seed.ts
+++ prisma-tenant-base/seed.ts
@@ -14,6 +14,7 @@ import {
   seedCountries,
   seedDepartments,
   seedMeasurementUnits,
+  seedCustomers,
 } from './seed/';
 
 async function main() {
@@ -40,6 +41,7 @@ async function main() {
     await seedRoleActions(prisma);
     await seedUserRoles(prisma);
     await seedItems(prisma);
+    await seedCustomers(prisma);
     await seedUserInfo(prisma);
 
     void prisma.$disconnect();
